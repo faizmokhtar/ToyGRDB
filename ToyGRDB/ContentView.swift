fbb7a8cf9079827d8b536298b22bc0488f89b22f
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-12-30 12:13:27 +0800

    Initial Commit

---
+++ ToyGRDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToyGRDB
+//
+//  Created by Faiz Mokhtar AD0502 on 30/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToyGRDB/ToyGRDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyGRDBApp.swift
+//  ToyGRDB
+//
+//  Created by Faiz Mokhtar AD0502 on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToyGRDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
